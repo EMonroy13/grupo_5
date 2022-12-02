aacfec560022b370206139cc42e5db3f1a0a80ba
gonzalomravicich <gonzalomravicich@gmail.com>
2022-12-02 19:19:43 -0300

    editando el header del home, solucionando error en static

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "../public"))); /* arreglando static */
 
 /* Necesitamos agregar app.listen method para probar en local */
 app.listen(3000, () => console.log('Server is running in http://localhost:3000'));
