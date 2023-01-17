f94e0424450dd42064c57255e4018582f08b2c85
gonzalomravicich <gonzalomravicich@gmail.com>
2023-01-16 22:31:25 -0300

    dandole dinamismo a la pag

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,8 +1,25 @@
 const path = require ("path");
-const fs = require ("fs")
+const fs = require ("fs");
+const { prependListener } = require("process");
+
+const productsFilePath = path.join(__dirname, '../database/products.json');
+const productosLeidos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const indexController = {
     index: (req, res)=>{
-        res.render(path.resolve(__dirname, "../views/index"))
+        const productosLeidos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        
+        const ultimoAgregado = productosLeidos[productosLeidos.length - 1];
+        const ultimosAgregados = productosLeidos.slice(productosLeidos.length -4);
+        const loMasVendido = productosLeidos.filter(producto=> producto.isTopSeller == true);
+        const algunasOfertas = productosLeidos.filter(producto=> producto.offer == true);
+        
+        res.render(path.resolve(__dirname, "../views/index"), {
+            ultimoAgregado:ultimoAgregado,
+            ultimosAgregados:ultimosAgregados,
+            loMasVendido : loMasVendido,
+            algunasOfertas : algunasOfertas
+        })
     }
 }
 
