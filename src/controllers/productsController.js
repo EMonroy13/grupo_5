08d05658f60cbf409bba1b286a439a291bba588d
luchito111 <lucianogverchelli@gmail.com>
2023-01-18 22:00:48 -0300

    arreglo para poder cargar todos los productos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,17 +2,29 @@ const path = require ("path");
 const fs = require ("fs")
 // constante para leer el json
 const productsFilePath = path.join(__dirname, "../database/products.json")
-const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8")) 
+// const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8")) 
 
 const productsController = {
     
     allProducts: (req, res)=>{
-        res.render(path.resolve(__dirname, "../views/allproducts"))
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+		
+        res.render(path.resolve(__dirname, "../views/allproducts"), {productos: products})
+       
     },
    
     
     productDetail: (req, res)=>{
-        res.render(path.resolve(__dirname, "../views/productDetail"))},
+        
+        let id = req.params.id;
+		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+		let productoFiltrado = products.find(producto => {
+			return producto.id == id
+		})
+        res.render(path.resolve(__dirname, "../views/productDetail"), {producto: productoFiltrado})
+    },
     
     
     create: (req, res)=>{
