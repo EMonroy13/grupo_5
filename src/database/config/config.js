6f727b83e261c54f0e549dc2425e5bcbf3c7fea8
EMonroy13 <edgarmore918@gmail.com>
2023-03-23 16:52:50 -0600

    Agregando configuración de la base en cloud

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "grupo_5",
-    "host": "127.0.0.1",
+    "username": "sql9608124",
+    "password": "EsQ9UivaSK",
+    "database": "sql9608124",
+    "host": "sql9.freesqldatabase.com",
+    "port": 3306,
     "dialect": "mysql"
   },
   "test": {
