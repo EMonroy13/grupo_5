129e9cdfe69d7e04f6aa8d75ca8f67daaa139396
gonzalomravicich <gonzalomravicich@gmail.com>
2023-03-24 20:39:57 -0300

    Solucionando errores en las validaciones de create

--- public/js/productAddValidation.js
+++ public/js/productAddValidation.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", ()=>{
     
 let formulario = document.querySelector(".form__upDate")
-let ulErrores = document.querySelector(".ul_errors");
+let ulErrores = document.querySelector(".ul_errores");
 
 formulario.addEventListener("submit", (event)=>{
 
@@ -17,36 +17,53 @@ errors.push("El nombre debe contener almenos 5 caracteres")
 formulario.productName.classList.add("is-invalid")
     } else {
         formulario.productName.classList.remove("is-invalid")
-        formulario.productName.classList.add("is-Valid")
+        formulario.productName.classList.add("is-valid")
     }
+//------------imagen -----------------
+
+ if(!(formulario.image.file)){
+        errors.push("Debe agregar una imagen")
+        formulario.image.classList.add("is-invalid")
+}
+
 //------------ descripcion -----------------
 if(formulario.description.value == ""){
     errors.push("El campo de descripcion es obligatorio")
-    formulario.description.classList.add("is-invalid")
+    formulario.description.style.border = "1px solid #f00;"
 }
 else if (formulario.description.value < 20){
-errors.push("El nombre debe contener almenos 20 caracteres")
-formulario.description.classList.add("is-invalid")
+errors.push("La descripcion debe contener almenos 20 caracteres");
+formulario.description.style.border = "1px solid #f00;"
 } else {
-    formulario.description.classList.remove("is-invalid")
-    formulario.description.classList.add("is-Valid")
+    formulario.description.classList.add("is-valid")
+    formulario.description.style.border = "1px solid #0f0;"
 }
+//-----------------categoria---------------
+if(formulario.category.value == "0"){
+    errors.push("Debe seleccionar una categoria")
+    formulario.productName.classList.add("is-invalid")
+}else {
+    formulario.productName.classList.remove("is-invalid")
+    formulario.productName.classList.add("is-valid")
+}
+
 // ------------- precio -------------
 if(formulario.price.value == ""){
-    errors.push("El campo de descripcion es obligatorio")
+    errors.push("Debe escribir el precio del producto")
     formulario.price.classList.add("is-invalid")
 }
 else if (formulario.price.value < 1){
-errors.push("El nombre debe contener almenos 1 cifra")
+errors.push("El precio debe contener almenos 1 cifra")
 formulario.price.classList.add("is-invalid")
 } else {
     formulario.price.classList.remove("is-invalid")
-    formulario.price.classList.add("is-Valid")
+    formulario.price.classList.add("is-valid")
 }
 
 if (errors.length > 0) {
 
     event.preventDefault();
+    
 ulErrores.innerHTML = "";
     errors.forEach(error => {
         ulErrores.innerHTML += "<li style='color: #f00;'>" + error + "</li>"
