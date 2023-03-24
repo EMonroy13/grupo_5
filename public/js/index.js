2ab969358f7ae90ebd0bd75ee272c536ca848421
gonzalomravicich <gonzalomravicich@gmail.com>
2023-03-24 14:28:59 -0300

    realizando funcionamiento carruzel de ultimos agregados en el index

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", ()=>{
+   let botonIzq = document.querySelector(".boton-izq-UA");
+   let botonDer = document.querySelector(".boton-der-UA");
+   let carrusel = document.querySelector(".ultimos-ingresos__contenedor")
+   botonDer.addEventListener("click",()=>{
+        carrusel.classList.add("ultimos-ingresos__carrusel")
+        botonDer.style.display = "none"
+        botonIzq.style.display = "block"
+        botonIzq.addEventListener("click",()=>{
+            carrusel.classList.remove("ultimos-ingresos__carrusel")
+            botonIzq.style.display = "none"
+            botonDer.style.display = "block"
+        })
+   })
+})
