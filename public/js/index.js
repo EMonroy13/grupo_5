72eab49798f5ea2dfda41b8af0ca26cf01f8f3c9
gonzalomravicich <gonzalomravicich@gmail.com>
2023-03-24 21:57:59 -0300

    funcionamiento del menu burger con js

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,8 @@ window.addEventListener("load", ()=>{
    let botonIzq = document.querySelector(".boton-izq-UA");
    let botonDer = document.querySelector(".boton-der-UA");
    let carrusel = document.querySelector(".ultimos-ingresos__contenedor")
+   let botonBurger = document.querySelector(".menu-burger")
+   let nabInferior = document.querySelector(".header__nav-inferior")
    botonDer.addEventListener("click",()=>{
         carrusel.classList.add("ultimos-ingresos__carrusel")
         botonDer.style.display = "none"
@@ -12,4 +14,7 @@ window.addEventListener("load", ()=>{
             botonDer.style.display = "block"
         })
    })
+   botonBurger.addEventListener("click",()=>{
+    nabInferior.classList.toggle("nav-inferior-show")
+    })
 })
