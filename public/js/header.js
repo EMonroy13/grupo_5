2dfcd71c47392f4f2a5148246931d129a927ef1a
gonzalomravicich <gonzalomravicich@gmail.com>
2023-03-27 01:38:33 -0300

    agregando animacion de cerrar session e ir al perfil

--- public/js/header.js
+++ public/js/header.js
@@ -1,7 +1,13 @@
 window.addEventListener("load", ()=>{
     let botonBurger = document.querySelector(".menu-burger")
     let nabInferior = document.querySelector(".header__nav-inferior")
+    let botonPerfil = document.querySelector(".boton-perfil");
+    let infoPerfil = document.querySelector(".datos-contenedor")
     botonBurger.addEventListener("click",()=>{
      nabInferior.classList.toggle("nav-inferior-show")
      })   
- })
+
+     botonPerfil.addEventListener("click",()=>{
+    infoPerfil.classList.toggle("show__perfil-contenedor")
+    })   
+})
