58fbf36aff3b648de5c22728af25fcc685e46518
Edgar Josafat Monroy Ruiz <edgar.monroy@imail2.org>
2023-03-30 10:37:38 -0600

    agregando config local

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,15 @@
 module.exports = {
   "development": {
-    "username": "sql9608124",
+    /* "username": "sql9608124",
     "password": "EsQ9UivaSK",
     "database": "sql9608124",
     "host": "sql9.freesqldatabase.com",
     "port": 3306,
+    "dialect": "mysql" */ 
+    "username": "root",
+    "password": null,
+    "database": "grupo_5",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
